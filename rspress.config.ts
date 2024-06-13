3535b42c37a1872c1633b689a420b141cc3c3181
whoimi <mail@whoimi.com>
2024-06-13 23:29:45 +0800

    init

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,6 +1,19 @@
+// import * as path from "path";
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-    // 文档根目录
-    root: 'docs',
+    // root: path.join(__dirname, "docs"),
+    root: "docs",
+    title: "pc-plus",
+    description: "Rspack-based Static Site Generator",
+    base: "/pc-plus",
+    themeConfig: {
+        socialLinks: [
+            {
+                icon: "github",
+                mode: "link",
+                content: "https://github.com/web-infra-dev/rspress",
+            },
+        ],
+    },
 });
